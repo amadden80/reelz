bcec6f62e798f4f0a28fc42bc787cdffc6fb2348
amadden80 <amadden80@gmail.com>
2014-10-10 13:59:40 -0400

    defines routes

--- app.rb
+++ app.rb
@@ -25,3 +25,40 @@ get '/movies/:id' do
   @tags = @movie.tags
   erb :"movies/show"
 end
+
+
+
+
+
+
+
+
+# index: Display all of a movie's comments
+get '/movies/:movie_id/comments' do
+  @movie = Movie.find(params[:movie_id])
+  @comments = Comment.all
+  erb :"comments/index"
+end
+
+# new: Return a form for a comment
+get '/movies/:movie_id/comments/new' do
+end
+
+# create: Create a new comment
+post '/movies/:movie_id/comments' do
+end
+
+# edit: Return a form to edit a comment
+get '/movies/:movie_id/comments/:id' do
+end
+# update: Update a comment
+patch '/movies/:movie_id/comments/:id' do
+end
+
+# show: Display a single comment
+get '/movies/:movie_id/comments/:id' do
+end
+
+# destroy: Delete a comment
+delete '/movies/:movie_id/comments/:id' do
+end
